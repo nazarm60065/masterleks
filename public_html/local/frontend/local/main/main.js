0921fac072078d0e3f4ca5e852ae805af267e59c
Юлия Русакова <ros.yuliya75@gmail.com>
2019-07-18 07:28:17 +0000

    Merged in icon (pull request #4)
    добавление иконки на сайт, корректировка разметки
    
    Approved-by: Kirill Granin <krllgranin@gmail.com>

--- public_html/local/frontend/local/main/main.js
+++ public_html/local/frontend/local/main/main.js
@@ -83,25 +83,6 @@ $(document).ready(function () {
 
     });
 });
-$(document).ready(function () {
-    $('.search__link').click(function() {
-        $('.input_search_area').toggleClass("show");
 
-    });
-    var button = $('.mobil-menu__block-button');
-    button.click(function(){
-
-        if(button.hasClass('opened')){
-            button.removeClass('opened');
-        }
-        else{
-            button.addClass('opened');
-        }
-    });
-    button.click(function() {
-        $('body').toggleClass("fix_overflow");
-        $('#mobile_menu').toggleClass("active");
-    });
-});
 
 
--- public_html/verstka/catalog.php
+++ public_html/verstka/catalog.php
@@ -5,21 +5,43 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>catalog</title>
     <link rel="stylesheet" type="text/css" href="/local/icons/icomoon/style.css">
-    <link rel="stylesheet" href="/local/frontend/local/main/main.css">
+    <link rel="stylesheet" href="/local/frontend/local/style/style.css">
     <link rel="stylesheet" href="/local/frontend/local/catalog/catalog.css">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,700i,900i|Roboto:400,500,700&display=swap&subset=cyrillic" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/local/js/jquery.slick/slick.css"/>
-    <link rel="icon" href=​"local/images/favicon.ico" type="image/x-icon">
+
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="Leks"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
 
     <script type="text/javascript" src="/local/js/jquery2/jquery-2.2.4.min.js"></script>
     <script type="text/javascript" src="/local/frontend/local/catalog/catalog.js"></script>
+    <script type="text/javascript" src="/local/frontend/local/style/style.js"></script>
 </head>
 <body>
 <header>
     <div class="full_section">
-        <div class="container container_flex">
+        <div class="container container_flex header__container">
             <div class="mobil-menu__block-button" onclick="toggle_menu(true)">
                 <span class="mobil-menu__button"></span>
             </div>
@@ -74,10 +96,10 @@
     <div class="footer-block">
         <div class="container">
             <div class="flex-container">
-                <div class="col-desk-50 d-tab-none  d-none">
+                <div class="col-desk-50 d-tablet-mobile-none">
                     <div class="footer_title"><a href="#" class="footer__title-link">Услуги</a></div>
                     <div class="flex-container">
-                        <div class="col-desk-50 d-tab-none">
+                        <div class="col-desk-50">
                             <div class="footer__nav">
                                 <ul class="footer__block-nav text-uppercase">
                                     <li class="footer__nav-element"><a href="#" class="footer__nav-link link-animation">ссылка на услугу</a></li>
@@ -88,7 +110,7 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-desk-50 d-tab-none">
+                        <div class="col-desk-50">
                             <div class="footer__nav">
                                 <ul class="footer__block-nav text-uppercase">
                                     <li class="footer__nav-element"><a href="#" class="footer__nav-link link-animation">ссылка на услугу</a></li>
@@ -101,7 +123,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-desk-25 d-tab-none d-none">
+                <div class="col-desk-25 d-tablet-mobile-none">
                     <div class="footer_title"><a href="#" class="footer__title-link">Блог</a></div>
                     <div class="footer__nav">
                         <ul class="footer__block-nav text-uppercase">
@@ -114,7 +136,7 @@
                     </div>
                 </div>
                 <div class="col-desk-25 col-tab-100">
-                    <div class="footer_title d-none"><a href="#" class="footer__title-link">Контакты</a></div>
+                    <div class="footer_title d-tablet-mobile-none"><a href="#" class="footer__title-link">Контакты</a></div>
                     <div class="footer__nav">
                         <ul class="footer__block-nav">
                             <li class="footer__nav-element footer__nav-element_text-big">Адресс</li>
