a6e63d5602d0cbf529e94b65a08436bb95df04f1
Makarov Oleg <f1ngerokk@gmail.com>
2019-07-25 13:12:16 +0900

    Добавление стилей для страницы 404

--- public_html/404.php
+++ public_html/404.php
@@ -2,29 +2,23 @@
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404", "Y");
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetAdditionalCSS('/local/frontend/local/404/404.css');
 $APPLICATION->SetTitle("Страница не найдена");
 ?>
-    <div class="full_section bg_yellow_2 initial_height">
-        <div class="container_section">
-            <div class="catalog">
-                <div class="left_part">
-                    <div class="h1 page_not_fount_title font_bold h1_bottom_margin">404</div>
-                    <div class="sub_title">Кажется что-то пошло не так!</div>
-                    <div class="sub_title">Извините, но мы не можем найти нужную Вам страницу.</div>
-                    <div class="sub_title">Скорее всего, Вы перешли по адресу, который больше не существует или не
-                        существовал никогда. Для продолжения перейдите на главную страницу сайта.
-                    </div>
-                    <!--<noindex>-->
-                    <a href="/" rel="nofollow" class="button">Перейти</a>
-                    <!--</noindex>-->
-                </div>
-                <div class="right_part">
-                    <img src="/images/bear.png" title="bear" alt="bear">
-                </div>
+    <div class="error_404_content">
+        <div class="left_part">
+            <div class="h1 page_not_fount_title">404</div>
+            <div class="sub_title">Кажется что-то пошло не так!</div>
+            <div class="sub_title">Извините, но мы не можем найти нужную Вам страницу.</div>
+            <div class="sub_title">Скорее всего, Вы перешли по адресу, который больше не существует или не
+                существовал никогда. Для продолжения перейдите на главную страницу сайта.
             </div>
+            <!--<noindex>-->
+            <a href="/" rel="nofollow" class="button">Перейти</a>
+            <!--</noindex>-->
+        </div>
+        <div class="right_part">
+            <img src="/local/images/bear.png" title="bear" alt="bear">
         </div>
     </div>
-    <script>
-        height_window_to_main("stretch");
-    </script>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
