db7165b0ac1606a0ae288a39b6f77840436407bb
Makarov Oleg <f1ngerokk@gmail.com>
2019-07-30 15:52:54 +0900

    добавление мета для разрешения экрана

--- public_html/local/templates/leks/header.php
+++ public_html/local/templates/leks/header.php
@@ -13,6 +13,7 @@ echo "<html lang=\"{$lang}\">";
 
 <head>
     <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
     <title><? $APPLICATION->ShowTitle() ?></title>
     <?
     $APPLICATION->ShowHead();
