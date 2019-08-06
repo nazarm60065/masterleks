95dae508f562bf10ac5d5ad85eb60e54b62f4c2c
Nazar Mansurov <n.mansurov@z-labs.ru>
2019-08-06 12:01:23 +0900

    картинка в первом блоке на всю ширину

--- public_html/local/templates/leks/components/zlabs/main.safe-include/top-block-main/template.php
+++ public_html/local/templates/leks/components/zlabs/main.safe-include/top-block-main/template.php
@@ -11,12 +11,14 @@ $arContext = [
 ]; ?>
 
 <section class="top-block">
-    <div class="container mobile_100">
+    <div class="mobile_100">
         <div class="top-block__img-block">
             <img class="top-block__img" src="<?= $arParams["~image"] ?>" alt="">
             <div class="top-block__title"><h1 class="top-block__title-text"><?= $arParams["~title"] ?></h1>
-                <div class="top-block__block-button">
-                    <a href="#order-form-modal" class="top-block__button feedback-form-link">Заказать быстро</a>
+                <div class="container top-block-container">
+                    <div class="top-block__block-button">
+                        <a href="#order-form-modal" class="top-block__button feedback-form-link">Заказать быстро</a>
+                    </div>
                 </div>
             </div>
         </div>
