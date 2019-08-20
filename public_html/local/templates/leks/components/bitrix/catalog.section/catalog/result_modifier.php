f57dc584b6930cf33beafedc643e8c71eb535517
d.agafonova <d.agafonova@z-labs.ru>
2019-08-20 10:25:36 +0900

    ресайз изображений в каталоге

--- public_html/local/templates/leks/components/bitrix/catalog.section/catalog/result_modifier.php
+++ public_html/local/templates/leks/components/bitrix/catalog.section/catalog/result_modifier.php
@@ -7,3 +7,32 @@
 
 $component = $this->getComponent();
 $arParams = $component->applyTemplateModifications();
+
+foreach ($arResult['ITEMS'] as $key => $item) {
+    if ($item['PREVIEW_PICTURE']) {
+        $picture = CFile::ResizeImageGet(
+            $item['~PREVIEW_PICTURE'],
+            array("width" => 350, "height" => 350),
+            BX_RESIZE_IMAGE_EXACT,
+            true
+        );
+        if ($picture) {
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE']['SRC'] = $picture['src'];
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE']['WIDTH'] = $picture['width'];
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE']['HEIGHT'] = $picture['height'];
+        }
+    }
+    if ($item['PREVIEW_PICTURE_SECOND'] && $item['DETAIL_PICTURE'] ) {
+        $pictureDetail = CFile::ResizeImageGet(
+            $item['~DETAIL_PICTURE'],
+            array("width" => 350, "height" => 350),
+            BX_RESIZE_IMAGE_EXACT,
+            true
+        );
+        if ($pictureDetail) {
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE_SECOND']['SRC'] = $pictureDetail ['src'];
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE_SECOND']['WIDTH'] = $pictureDetail ['width'];
+            $arResult['ITEMS'][$key]['PREVIEW_PICTURE_SECOND']['HEIGHT'] = $pictureDetail ['height'];
+        }
+    }
+}
