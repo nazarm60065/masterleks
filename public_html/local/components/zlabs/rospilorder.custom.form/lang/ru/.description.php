81b82a29b09e36fa825c83ea480f3b82fa73e238
d.agafonova <d.agafonova@z-labs.ru>
2019-08-20 16:00:25 +0900

    обновление формы поиска товара

--- public_html/local/components/zlabs/rospilorder.custom.form/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$MESS['ROSPILORDER_FORM_NAME'] = 'Форма поиска заказа';
-$MESS['ROSPILORDER_FORM_DESCRIPTION'] = 'Форма поиска заказа';
-$MESS['ROSPILORDER_FORM_GROUP'] = 'Z-Labs';
-$MESS['ROSPILORDER_FORM_CHILD_GROUP'] = 'Роспил';
