b920bd539bf77f6828906749a8012e24ae6cf567
Anton Rikhnovets <a.rihnovec@z-labs.ru>
2019-11-12 12:43:51 +0900

    Добавил новый шаблон для компонента zlabs.rospilorder

---
+++ public_html/local/templates/leks/components/zlabs/rospilorder.form/order-form/template.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+
+$mustache = new Mustache_Engine(['loader' => new Mustache_Loader_FilesystemLoader(__DIR__)]);
+echo $mustache->render('template', $arResult);
