53098598c30ddf4f655b8d30d0ecbf4c880b68e5
bb@z-labs.ru <bb@z-labs.ru>
2020-06-22 11:14:24 +0300

    Production changes

--- public_html/local/include_areas/footer_email.php
+++ public_html/local/include_areas/footer_email.php
@@ -1,4 +1,1 @@
-<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
-
-<a href="mailto:sales@masterleks.ru" class="footer__nav-link"
-   title="Электронная почта отдела продаж">sales@masterleks.ru</a>
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>&nbsp; <a href="mailto:546767@masterleks.ru" target="_blank">546767@masterleks.ru</a>&nbsp;
