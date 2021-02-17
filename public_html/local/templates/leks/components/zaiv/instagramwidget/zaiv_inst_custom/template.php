71af537699e808ffdfc7f23f3568ef79b93546a0
toropova rita <toropova.rita@gmail.com>
2021-02-17 09:50:51 +0900

    37622 заголовки в другие стороны

--- public_html/local/templates/leks/components/zaiv/instagramwidget/zaiv_inst_custom/template.php
+++ public_html/local/templates/leks/components/zaiv/instagramwidget/zaiv_inst_custom/template.php
@@ -1,11 +1,10 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 <?if($arParams[NOINDEX_WIDGET]=="Y"){?><!--googleoff: all--><!--noindex--><?}?>
 <div class="zaiv-instagram-widget-container">
-
+    <div class="section__sub-title section__sub-title_right">
+        Наш Instagram
+    </div>
 	<?if(!count($arResult[ERRORS])){?>
-        <div class="section__sub-title section__sub-title_mobile-center">
-            Наш Instagram
-        </div>
 	<div class="zaiv-instagram-widget-media w<?=($arParams[MEDIA_ROW_COUNT])?$arParams[MEDIA_ROW_COUNT]:"8"?> <?=($arParams[MEDIA_ROW_COUNT]>5)?"wide":""?>">
 			<?foreach($arResult[ITEMS] as $arItem){?>
 				<a href="<?=$arItem[LINK]?>" <?=($arParams[NOINDEX_LINKS]=="Y")?'rel="nofollow"':''?> target="_blank" style="background-image:url(<?=$arItem[IMAGE]?>)"></a>
